83ab56163582e711233a671c729eee4c827c2a09
6cassio1b <santos.silva.cassio@escola.pr.gov.br>
2026-07-31 00:28:39 -0300

    index

--- scrip.js
+++ scrip.js
@@ -1,41 +1,45 @@
-const likes = document.querySelectorAll(".like");
-const deslikes = document.querySelectorAll(".deslike");
-const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+const likes=document.querySelectorAll(".like");
+const deslikes=document.querySelectorAll(".deslike");
 
 likes.forEach(function(botao){
-let curtido = false;
-
-botao.addEventListener("click", function(){
-const contador = botao.querySelector("span");
-let numero = Number(contador.textContent);
-
-if(curtido === false){
-contador.textContent = numero + 1;
-curtido = true;
+let curtido=false;
+botao.addEventListener("click",function(){
+let texto=botao.querySelector("span");
+let numero=Number(texto.textContent);
+if(!curtido){
+texto.textContent=numero+1;
+curtido=true;
 }else{
-contador.textContent = numero - 1;
-curtido = false;
+texto.textContent=numero-1;
+curtido=false;
 }
 });
 });
 
 deslikes.forEach(function(botao){
-let descurtido = false;
-
-botao.addEventListener("click", function(){
-const contador = botao.querySelector("span");
-let numero = Number(contador.textContent);
-
-if(descurtido === false){
-contador.textContent = numero + 1;
-descurtido = true;
+let descurtido=false;
+botao.addEventListener("click",function(){
+let texto=botao.querySelector("span");
+let numero=Number(texto.textContent);
+if(!descurtido){
+texto.textContent=numero+1;
+descurtido=true;
 }else{
-contador.textContent = numero - 1;
-descurtido = false;
+texto.textContent=numero-1;
+descurtido=false;
 }
 });
 });
 
-btnTemaEscuro.addEventListener("click", function(){
-document.documentElement.classList.toggle("tema-escuro");
-});
+const btnTemaEscuro=document.querySelector(".btn-tema-escuro");
+
+btnTemaEscuro.addEventListener("click",mudaTema);
+
+function mudaTema(){
+const corpo=document.body;
+if(corpo.classList.contains("tema-escuro")){
+corpo.classList.remove("tema-escuro");
+}else{
+corpo.classList.add("tema-escuro");
+}
+}
